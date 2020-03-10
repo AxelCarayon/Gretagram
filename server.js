d4891c10976ae42729b9e375763ad040d4f88f94
AxelCarayon <axel.carayon@etud.univ-jfc.fr>
2020-03-10 10:10:29 +0100

    Fix chemins

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/testUser', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/tests/testUser.html'));
+    res.sendFile(path.join(__dirname + '/public/tests/testsBDD.html'));
 });
 
 app.get('/showUser', function(req, res) {
