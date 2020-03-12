a88574c45c2fb0ab3312548435e17dda0f0758a7
AxelCarayon <axel.carayon@etud.univ-jfc.fr>
2020-03-12 16:28:16 +0100

    Ajout d'un API exemple

--- api-routes.js
+++ api-routes.js
@@ -1,12 +1,25 @@
 // Filename: api-routes.js
 // Initialize express router
 let router = require('express').Router();
+
 // Set default API response
 router.get('/', function(req, res) {
     res.json({
         status: 'API en marche',
-        message: 'API de Gretagram fonctionne :)'
+        message: 'API RESTful de Gretagram'
     });
 });
+
+// Import contact controller
+var contactController = require('./controller/contactController'); // Contact routes
+router.route('/contacts')
+    .get(contactController.index)
+    .post(contactController.new);
+router.route('/contacts/:contact_id')
+    .get(contactController.view)
+    .patch(contactController.update)
+    .put(contactController.update)
+    .delete(contactController.delete);
+
 // Export API routes
 module.exports = router;
