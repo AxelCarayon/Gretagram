24fcb1aba45d18d9ce033399c9d974cdac526fdc
cgobbo <corentin.gobbo@hotmail.fr>
2020-05-03 17:30:11 +0200

    mise en place recherche coté front + maj option publications

--- public/Controller/js/js-beautify.js
+++ public/Controller/js/js-beautify.js
@@ -88,4 +88,10 @@ $(document).ready(() => {
         
     })
 
+    $(window).scroll(function() {   
+        if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+            console.log("botoom");
+            
+        }
+     });
 });
