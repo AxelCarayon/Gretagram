c95541fc94e2f55220ee4b72d5d4fb27508fc619
AxelCarayon <axel.carayon@etud.univ-jfc.fr>
2020-05-03 16:46:54 +0200

    ajout des # dans le peuplement de la BDD et dans la recherche

--- api/api-routes.js
+++ api/api-routes.js
@@ -48,6 +48,8 @@ router.route('/publication/proche')
 router.route('/publication/zone')
     .get(publicationController.carre);
 
+router.route('/recherche/hashtag')
+    .get(hashtagController.search);
 
 router.route('/publication/all')
     .get(publicationController.all);
